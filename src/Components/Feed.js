53bebed796622e9d97bb3d4762502e7cbc7ff613
IhsanulAfkar <ihsanul2001@gmail.com>
2024-04-18 11:01:06 +0700

    add new projects

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -13,29 +13,38 @@ export const Feed = ({ handleClick }) => {
         },
         {
             id: 2,
-            img: '/project/2.png',
-            tag: 'Web Development, Project Manager, Comitee',
-            title: 'Chief of Website Development ARA 4.0',
-            desc: "Determine web framework, Develop both front-end and back-end, Handling staff divisions and jobs, and Manage the hosted server for ARA 4.0.",
-            url: 'https://www.instagram.com/p/CkiC0bvpgF4/'
-        },
-        {
-            id: 3,
             img: '/project/3.png',
             tag: 'Web Development, Staff, Comitee',
             title: 'Staff of Website Development ARA 2023',
             desc: "Assigned to handle back-end using codeigniter framework in ARA 2023.",
             url: 'https://www.instagram.com/p/CfD90TzJ8CI/'
+        },
+        {
+            id: 3,
+            img: '/project/2.png',
+            tag: 'Web Development, Project Manager, Comitee',
+            title: 'Chief of Website Development ARA 4.0',
+            desc: "Determine web framework, Develop both front-end and back-end, Handling staff divisions and jobs, and Manage the hosted server for ARA 4.0.",
+            url: 'https://www.instagram.com/p/CkiC0bvpgF4/'
+
+        },
+        {
+            id: 4,
+            img: '/project/4.png',
+            tag: 'Web Development, Internship',
+            title: 'Web Developer Internship in PT Adma Digital Solusi (ADS Digital Partner)',
+            desc: "Create and develop a product called Forwardin in teams as a Web Developer",
+            url: 'https://www.instagram.com/forwardin.id/'
         }
     ]
 
     return (
         <div className='mt-16 sm:mt-24' id='project' data-aos="fade-right">
             <p className='text-bitWhite text-2xl font-bold text-center sm:text-left lg:text-4xl'>
-                My Projects and <br className='md:hidden' /> Accomplishments
+                My Projects and <br className='md:hidden' /> Internships
             </p>
             <div className='mt-6 sm:mt-12 lg:w-[75%]'>
-                {myProjects.map(data => {
+                {myProjects.reverse().map(data => {
                     return (
                         <Project key={data.id} data={data} handleClick={handleClick} />
                     )
